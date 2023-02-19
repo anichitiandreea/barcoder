079aba843b0ebb96fc41482b843c149b8c0fd23e
Andreea Anichiti <andreea.anichiti@gmail.com>
2023-02-19 21:35:04 +0200

    refactor: move head to separate component

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,10 @@
 import Navbar from './navbar';
+import Head from './head';
 
 export default function Layout({ children }) {
   return (
     <div>
+      <Head/>
       <Navbar/>
       {children}
     </div>
